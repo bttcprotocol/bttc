881ce75fe1bad85888a2655fbe4f4f5e44e5ebf2
halibobo1205 <halibobo1205@gmail.com>
2021-11-04 18:11:49 +0800

    update go.mod for github.com/Azure/azure-stora ge-blob-go:fix

--- internal/build/azure.go
+++ internal/build/azure.go
@@ -39,7 +39,7 @@ type AzureBlobstoreConfig struct {
 // need a multi API call approach implemented.
 //
 // See: https://msdn.microsoft.com/en-us/library/azure/dd179451.aspx#Anchor_3
-func AzureBlobstoreUpload(path string, name string, config AzureBlobstoreConfig) error {
+func AzureBlobstoreUpload(path string, name string, config AzureBlobstoreConfig, cpk azblob.ClientProvidedKeyOptions) error {
 	if *DryRunFlag {
 		fmt.Printf("would upload %q to %s/%s/%s\n", path, config.Account, config.Container, name)
 		return nil
@@ -65,12 +65,14 @@ func AzureBlobstoreUpload(path string, name string, config AzureBlobstoreConfig)
 	}
 	defer in.Close()
 
-	_, err = blockblob.Upload(context.Background(), in, azblob.BlobHTTPHeaders{}, azblob.Metadata{}, azblob.BlobAccessConditions{})
+	_, err = blockblob.Upload(context.Background(), in, azblob.BlobHTTPHeaders{},
+		azblob.Metadata{}, azblob.BlobAccessConditions{}, azblob.AccessTierNone,
+		nil, cpk)
 	return err
 }
 
 // AzureBlobstoreList lists all the files contained within an azure blobstore.
-func AzureBlobstoreList(config AzureBlobstoreConfig) ([]azblob.BlobItem, error) {
+func AzureBlobstoreList(config AzureBlobstoreConfig) ([]azblob.BlobItemInternal, error) {
 	credential := azblob.NewAnonymousCredential()
 	if len(config.Token) > 0 {
 		c, err := azblob.NewSharedKeyCredential(config.Account, config.Token)
@@ -84,7 +86,7 @@ func AzureBlobstoreList(config AzureBlobstoreConfig) ([]azblob.BlobItem, error) 
 	u, _ := url.Parse(fmt.Sprintf("https://%s.blob.core.windows.net", config.Account))
 	service := azblob.NewServiceURL(*u, pipeline)
 
-	var allBlobs []azblob.BlobItem
+	var allBlobs []azblob.BlobItemInternal
 	// List all the blobs from the container and return them
 	container := service.NewContainerURL(config.Container)
 	nextMarker := azblob.Marker{}
@@ -104,7 +106,7 @@ func AzureBlobstoreList(config AzureBlobstoreConfig) ([]azblob.BlobItem, error) 
 
 // AzureBlobstoreDelete iterates over a list of files to delete and removes them
 // from the blobstore.
-func AzureBlobstoreDelete(config AzureBlobstoreConfig, blobs []azblob.BlobItem) error {
+func AzureBlobstoreDelete(config AzureBlobstoreConfig, blobs []azblob.BlobItemInternal) error {
 	if *DryRunFlag {
 		for _, blob := range blobs {
 			fmt.Printf("would delete %s (%s) from %s/%s\n", blob.Name, blob.Properties.LastModified, config.Account, config.Container)
